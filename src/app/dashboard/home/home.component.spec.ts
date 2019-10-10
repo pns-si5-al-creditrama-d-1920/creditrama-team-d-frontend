25dffc6082a4b824a4e21fe8bdc9ee59275fd7e3
Theo Foray <theo.foray@etu.unice.fr>
2019-10-10 15:41:52 +0200

    Component test example for  UX element ( including a homemade one)

---
+++ src/app/dashboard/home/home.component.spec.ts
@@ -0,0 +1,55 @@
+import { async, ComponentFixture, TestBed, inject, tick, fakeAsync } from '@angular/core/testing';
+
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
+import { HomeComponent } from './home.component';
+import { NavbarComponent } from 'app/shared/navbar/navbar.component';
+import { FigurecardComponent } from 'app/shared/figurecard/figurecard.component';
+import { ImagecardComponent } from 'app/shared/imagecard/imagecard.component';
+import { MatFormField } from '@angular/material/form-field';
+import { MsgIconBtnComponent } from 'app/shared/msgiconbtn/msgiconbtn.component';
+import { MatMenuModule } from '@angular/material/menu';
+import { ObserversModule } from '@angular/cdk/observers';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+describe('HomeComponent', () => {
+	let component: HomeComponent;
+	let fixture: ComponentFixture<HomeComponent>; // Test environment for the component
+	let de: DebugElement; // Rendered HTML part of the component in its environment
+
+	beforeEach(
+		async(() => {
+			TestBed.configureTestingModule({
+				// specific NgModule for this testing env (eg. where to add the tested components and services)
+				declarations: [
+					HomeComponent,
+					NavbarComponent,
+					FigurecardComponent,
+					ImagecardComponent,
+					MsgIconBtnComponent
+				],
+				imports: [ MatMenuModule, ObserversModule, MatInputModule, BrowserAnimationsModule ]
+			}).compileComponents();
+		})
+	);
+
+	beforeEach(() => {
+		// Instantiate the context with the tested component
+		fixture = TestBed.createComponent(HomeComponent);
+		component = fixture.componentInstance;
+		de = fixture.debugElement;
+
+		fixture.detectChanges();
+	});
+
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+
+	it('should have navbar titled Dashboard', () => {
+		expect(de.query(By.css('app-navbar')).nativeElement.title).toBe('Dashboard');
+	});
+});
