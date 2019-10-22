9b8ba13828670e2d89be517d2e6541222c46254d
Nathan Strobbe <nathan.strobbe@etu.unice.fr>
2019-10-22 16:55:15 +0200

    Fix mainAccount variable

--- src/app/dashboard/home/home.component.ts
+++ src/app/dashboard/home/home.component.ts
@@ -1,37 +1,31 @@
 import {Component, OnInit} from '@angular/core';
-import {BankService} from 'app/services/bank.service';
-import {Observable} from 'rxjs';
-import {BankAccount} from '../../shared/model/bank-account';
-import {OAuthService} from 'angular-oauth2-oidc';
 import {ActivatedRoute} from '@angular/router';
 import {User} from '../../models/user';
 import {AuthService} from '../../services/auth.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  authUser: User;
+    authUser: User;
+    mainAccount: number;
 
-  constructor(private auth: AuthService, private route: ActivatedRoute) {
-  }
+    constructor(private auth: AuthService, private route: ActivatedRoute) {
+    }
 
-  ngOnInit() {
-	// this.balance = this.accounts[0]['balance'];
-	this.route.data.subscribe(data => this.auth.getAuthUser().subscribe(v => {
-		console.log(v);
-		this.authUser = v;
-		}
-	));
-  }
+    ngOnInit() {
+        // this.balance = this.accounts[0]['balance'];
+        this.route.data.subscribe(data => this.auth.getAuthUser().subscribe(v => {
+                console.log(v);
+                this.authUser = v;
+                this.mainAccount = this.authUser.bankAccounts[0]['id'];
+            }
+        ));
+    }
 
-  /*async fetchData() {
-
-    }*/
-
-  show() {
-	console.log(this.authUser);
-  }
+    show() {
+        console.log(this.authUser);
+    }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+    production: true,
+    AUTH_SERVER: 'http://localhost:9191/',
+    BANK_SERVICE_URL: 'http://localhost:8080/',
+    BASIC_USER: 'mobile',
+    BASIC_PASS: 'pin'
 };
