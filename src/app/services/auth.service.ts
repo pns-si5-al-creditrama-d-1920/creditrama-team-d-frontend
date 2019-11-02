66e1764eaa4377135f3dd8ff67258a6812ae3ee8
Laura Lopez <laura.lopez1@etu.unice.fr>
2019-11-02 16:44:19 +0100

    add few more tests (not finished yet)

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -15,45 +15,47 @@ export class AuthService {
 
   constructor(private http: HttpClient, private oauthService: OAuthService, private router: Router) {
   }
+
   authUser: Observable<User>;
 
   public isAuthenticated(): boolean {
-	const seconds = new Date().getTime() / 1000;
-	if (this.oauthService.hasValidAccessToken()) {
-		if (seconds - this.oauthService.getAccessTokenExpiration() < 0) {
-		return true;
-		}
-	}
-	return false;
+    const seconds = new Date().getTime() / 1000;
+    if (this.oauthService.hasValidAccessToken()) {
+      if (seconds - this.oauthService.getAccessTokenExpiration() < 0) {
+        return true;
+      }
+    }
+    return false;
   }
 
   login(username: string, password: string) {
 
-	this.oauthService.fetchTokenUsingPasswordFlowAndLoadUserProfile(username, password).then(() => {
-	  this.oauthService.getIdentityClaims();
-		this.router.navigate(['/dashboard']);
-	});
+    this.oauthService.fetchTokenUsingPasswordFlowAndLoadUserProfile(username, password).then(() => {
+      console.log('here');
+      this.oauthService.getIdentityClaims();
+      this.router.navigate(['/dashboard']);
+    });
 
   }
 
   public getAuthUser(act: boolean = false): Observable<User> {
-	if (!this.authUser || act) {
-	  console.log(environment.BANK_SERVICE_URL);
-		this.authUser = this.http.get<User>(environment.BANK_SERVICE_URL + 'clients/auth').pipe(
-		publishReplay(1), // this tells Rx to cache the latest emitted
-		refCount() // and this tells Rx to keep the Observable alive as long as there are any Subscribers
-		);
-	}
-	return this.authUser;
+    if (!this.authUser || act) {
+      console.log(environment.BANK_SERVICE_URL);
+      this.authUser = this.http.get<User>(environment.BANK_SERVICE_URL + 'clients/auth').pipe(
+        publishReplay(1), // this tells Rx to cache the latest emitted
+        refCount() // and this tells Rx to keep the Observable alive as long as there are any Subscribers
+      );
+    }
+    return this.authUser;
   }
 
   clearCache() {
-	this.authUser = null;
+    this.authUser = null;
   }
 
   logout() {
-	this.clearCache();
-	// remove user from local storage to log user out
-	this.oauthService.logOut();
+    this.clearCache();
+    // remove user from local storage to log user out
+    this.oauthService.logOut();
   }
 }
