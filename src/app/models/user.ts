8d806b8fd2b3a1ec50abfaab7d4ee9975aab4c47
Laura Lopez <laura.lopez1@etu.unice.fr>
2019-11-02 15:46:43 +0100

    add email to user, add register and createBankAccount methods in bank service

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,11 +1,12 @@
-import { BankAccount } from '../shared/model/bank-account';
-import { BankTransaction } from 'app/shared/model/bank-transaction';
+import {BankAccount} from '../shared/model/bank-account';
+import {BankTransaction} from 'app/shared/model/bank-transaction';
 
 export interface User {
-	userId: number;
-	username: string;
-	password: string;
-	bankAccounts: BankAccount[];
-	recipients: number[];
-	transactions: BankTransaction[];
+  userId: number;
+  username: string;
+  password: string;
+  email: string;
+  bankAccounts: BankAccount[];
+  recipients: number[];
+  transactions: BankTransaction[];
 }
--- src/app/services/bank.service.ts
+++ src/app/services/bank.service.ts
@@ -1,34 +1,51 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { URL_SERVER } from '../shared/constants/urls';
-import { Observable } from 'rxjs';
-import { BankTransaction } from 'app/shared/model/bank-transaction';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {URL_SERVER} from '../shared/constants/urls';
+import {Observable} from 'rxjs';
+import {BankTransaction} from 'app/shared/model/bank-transaction';
+import {User} from "../models/user";
 
 @Injectable({
-	providedIn: 'root'
+  providedIn: 'root'
 })
 export class BankService {
-	private route = '/bank';
+  private route = '/bank';
 
-	constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+  }
 
-	getBankAccount(userId: number): Observable<any> {
-		return this.http.get(URL_SERVER + this.route + '/' + userId + '/bank/accounts');
-	}
+  getBankAccount(userId: number): Observable<any> {
+    return this.http.get(URL_SERVER + this.route + '/clients/' + userId + '/bank-accounts');
+  }
 
-	updateRecipients(userId: number, newRecipient: number) {
-		console.log('updateRecipients service');
-		console.log(userId);
-		console.log(newRecipient);
-		console.log(URL_SERVER + this.route + '/' + 'clients/' + userId + '/recipients');
-		return this.http.post(URL_SERVER + this.route + '/' + 'clients/' + userId + '/recipients', newRecipient);
-	}
+  addBankAccount(userId: number, balance: number): Observable<any> {
+    return this.http.post(URL_SERVER + this.route + '/clients/' + userId + '/bank-accounts', balance);
+  }
 
-	transfer(userId: number, transaction: BankTransaction) {
-		return this.http.post(URL_SERVER + this.route + '/clients/' + userId + '/transactions', transaction);
-	}
+  updateRecipients(userId: number, newRecipient: number) {
+    console.log('updateRecipients service');
+    console.log(userId);
+    console.log(newRecipient);
+    console.log(URL_SERVER + this.route + '/' + 'clients/' + userId + '/recipients');
+    return this.http.post(URL_SERVER + this.route + '/' + 'clients/' + userId + '/recipients', newRecipient);
+  }
 
-	dump() {
-		return this.http.get(URL_SERVER + '/dump');
-	}
+  transfer(userId: number, transaction: BankTransaction) {
+    return this.http.post(URL_SERVER + this.route + '/clients/' + userId + '/transactions', transaction);
+  }
+
+  register(userName: string, userMail: string, userPassword: string) {
+    return this.http.post(URL_SERVER + '/register', JSON.stringify({
+      id: 0,
+      username: userName,
+      email: userMail,
+      password: userPassword
+    })).subscribe((user: User) => {
+      this.addBankAccount(user.userId, 100);
+    });
+  }
+
+  dump() {
+    return this.http.get(URL_SERVER + '/dump');
+  }
 }
