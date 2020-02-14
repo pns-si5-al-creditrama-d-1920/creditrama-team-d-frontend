17828e3ec2af5e0e001382b2b1de7d8fe8bfc778
Theo Foray <theo.foray@etu.unice.fr>
2020-02-14 16:29:22 +0100

    #35 temporary tables for pending transactions

--- src/app/dashboard/history/history.component.ts
+++ src/app/dashboard/history/history.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {MatTableDataSource} from '@angular/material/table';
 import {BankTransaction} from 'app/models/bank-transaction';
+import {BankTransactionService} from '../../services/bank-transaction.service';
+import {BankTransactionResponse} from "../../models/bank-transaction-response";
 
 
 @Component({
@@ -32,11 +34,90 @@ export class HistoryComponent implements OnInit {
     'button'
   ];
 
-  constructor() { }
+  codes: number[];
+
+  constructor(private bankTransactionService: BankTransactionService) { }
 
   ngOnInit() {
     this.dataSourceTransactions = new MatTableDataSource<BankTransaction>(this.transactions);
-    console.log(this.pending);
   }
+
+  transfer(index: number) {
+    console.log(this.codes[index]);
+    const transaction = {
+      amount: this.transactions[index].amount,
+      ibanSource: this.transactions[index].source.iban,
+      ibanDest: this.transactions[index].dest.iban,
+    }
+    this.bankTransactionService.makeTransaction(transaction).subscribe(
+      (transactionResponse: BankTransactionResponse) => {
+        this.bankTransactionService.confirmCode(transactionResponse.uuid, this.codes[index]).subscribe(
+          (response) => {
+            console.log(response);
+          },
+          (error) => {
+            console.error(error);
+          });
+      }
+    );
+    //console.log(this.thirdFormGroup.getRawValue());
+    /*const transaction = {
+      amount: this.thirdFormGroup.getRawValue().amount,
+      ibanSource: this.firstFormGroup.getRawValue().sourceId,
+      ibanDest: this.secondFormGroup.getRawValue().destinationId,
+    };*/
+
+    //console.log(transaction);
+    /*this.bankTransactionService.makeTransaction(transaction).subscribe(
+        (transactionResponse: BankTransactionResponse) => {
+		  this.auth.getAuthUser(true);
+		  transactionResponse.code
+		  this.bankTransactionService.confirmCode(transactionResponse.uuid, code).subscribe(
+			(response) => {
+			  if (response) {
+				swal({
+				  title: 'Success',
+				  text: 'Transaction acceptée !',
+				  confirmButtonClass: 'btn btn-success'
+				});
+			  }
+			},
+			(error) => {
+			  console.error(error);
+			}
+          if (!transactionResponse.code) {
+            this.router.navigate(['./dashboard']);
+          } else {
+            swal({
+              title: 'Confirmation du code',
+              input: 'text',
+              inputAttributes: {
+                autocapitalize: 'off'
+              },
+              showCancelButton: false,
+              confirmButtonText: 'Confirmer',
+              showLoaderOnConfirm: true,
+              preConfirm: (code) => {
+                console.log(code);
+                this.bankTransactionService.confirmCode(transactionResponse.uuid, code).subscribe(
+                    (response) => {
+                      if (response) {
+                        this.router.navigate(['./dashboard']);
+                        swal({
+                          title: 'Success',
+                          text: 'Transaction acceptée !',
+                          confirmButtonClass: 'btn btn-success'
+                        });
+                      }
+                    },
+                    (error) => {
+                      console.error(error);
+                    });
+              },
+              allowOutsideClick: () => !swal.isLoading()
+            });
+          }
+    });*/
+	}
 
 }
