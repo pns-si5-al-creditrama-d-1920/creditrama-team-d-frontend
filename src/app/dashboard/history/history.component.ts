f090e531f50808289640ca5248db6b3b9af6e5a7
Nathan Strobbe <nathan.strobbe@etu.unice.fr>
2020-02-14 19:31:46 +0100

    Fix confirm code communication with SAGA (#35)

--- src/app/dashboard/history/history.component.ts
+++ src/app/dashboard/history/history.component.ts
@@ -42,82 +42,13 @@ export class HistoryComponent implements OnInit {
     this.dataSourceTransactions = new MatTableDataSource<BankTransaction>(this.transactions);
   }
 
-  transfer(index: number) {
-    console.log(this.codes[index]);
-    const transaction = {
-      amount: this.transactions[index].amount,
-      ibanSource: this.transactions[index].source.iban,
-      ibanDest: this.transactions[index].dest.iban,
-    }
-    this.bankTransactionService.makeTransaction(transaction).subscribe(
-      (transactionResponse: BankTransactionResponse) => {
-        this.bankTransactionService.confirmCode(transactionResponse.uuid, this.codes[index]).subscribe(
-          (response) => {
-            console.log(response);
-          },
-          (error) => {
-            console.error(error);
-          });
-      }
-    );
-    //console.log(this.thirdFormGroup.getRawValue());
-    /*const transaction = {
-      amount: this.thirdFormGroup.getRawValue().amount,
-      ibanSource: this.firstFormGroup.getRawValue().sourceId,
-      ibanDest: this.secondFormGroup.getRawValue().destinationId,
-    };*/
-
-    //console.log(transaction);
-    /*this.bankTransactionService.makeTransaction(transaction).subscribe(
-        (transactionResponse: BankTransactionResponse) => {
-		  this.auth.getAuthUser(true);
-		  transactionResponse.code
-		  this.bankTransactionService.confirmCode(transactionResponse.uuid, code).subscribe(
-			(response) => {
-			  if (response) {
-				swal({
-				  title: 'Success',
-				  text: 'Transaction acceptée !',
-				  confirmButtonClass: 'btn btn-success'
-				});
-			  }
-			},
-			(error) => {
-			  console.error(error);
-			}
-          if (!transactionResponse.code) {
-            this.router.navigate(['./dashboard']);
-          } else {
-            swal({
-              title: 'Confirmation du code',
-              input: 'text',
-              inputAttributes: {
-                autocapitalize: 'off'
-              },
-              showCancelButton: false,
-              confirmButtonText: 'Confirmer',
-              showLoaderOnConfirm: true,
-              preConfirm: (code) => {
-                console.log(code);
-                this.bankTransactionService.confirmCode(transactionResponse.uuid, code).subscribe(
-                    (response) => {
-                      if (response) {
-                        this.router.navigate(['./dashboard']);
-                        swal({
-                          title: 'Success',
-                          text: 'Transaction acceptée !',
-                          confirmButtonClass: 'btn btn-success'
-                        });
-                      }
-                    },
-                    (error) => {
-                      console.error(error);
-                    });
-              },
-              allowOutsideClick: () => !swal.isLoading()
-            });
-          }
-    });*/
-	}
-
+  transfer(index) {
+    this.bankTransactionService.confirmCode(this.transactions[index].uuid, this.codes[index]).subscribe(
+        (response) => {
+          console.log(response);
+        },
+        (error) => {
+          console.error(error);
+        });
+  }
 }
