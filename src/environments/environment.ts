c9ffbe13920c88f1cbfa3d9a3433dd5bc4c6cdd6
Nathan Strobbe <nathan.strobbe@etu.unice.fr>
2020-02-18 15:41:59 +0100

    Oops fix environment variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,10 @@
-
 export const environment = {
   production: false,
-  AUTHORIZATION_SERVICE_URL: 'http://creditrama-team-d-authorization-service-al-team-d.apps.jboss.team/',
-  CLIENT_SERVICE_URL: 'http://creditrama-team-d-client-service-al-team-d.apps.jboss.team/',
-  TRANSACTION_SERVICE_URL: 'http://creditrama-team-d-transaction-service-al-team-d.apps.jboss.team/',
-  BANKACCOUNT_SERVICE_URL: 'http://creditrama-team-d-bankaccount-service-al-team-d.apps.jboss.team/',
-  CARD_SERVICE_URL: 'http://creditrama-team-d-card-service-al-team-d.apps.jboss.team/',
+  AUTHORIZATION_SERVICE_URL: 'http://localhost:9191/',
+  CLIENT_SERVICE_URL: 'http://localhost:8080/',
+  TRANSACTION_SERVICE_URL: 'http://localhost:8084/',
+  BANKACCOUNT_SERVICE_URL: 'http://localhost:8083/',
+  CARD_SERVICE_URL: 'http://localhost:8085/',
   BASIC_USER: 'mobile',
   BASIC_PASS: 'pin',
 };
-
